dae6b9ffcc7402dd0af94b8d8474df619de24f71
Rahul <rahulna@thoughtworks.com>
2016-07-22 23:15:04 +0530

    Linked List is implemented

--- data_structures/structs.go
+++ data_structures/structs.go
@@ -1,13 +1,45 @@
 package data_structures
 
 // Linked List Implementation
+type LinkedList struct{
+	first *Node
+	last *Node
+	length int
+}
 
-type Node struct{}
+type Node struct{
+	element int
+	next *Node
+	prev *Node
+}
+
+func (this *LinkedList) addElement(elem int){
+	anotherNode := Node{element : elem, next : nil, prev: this.last}
+
+	if this.first == nil {
+		this.first = &anotherNode
+	} else {
+		this.last = &anotherNode	
+	}
+	this.length += 1
+}
+
+func (this *LinkedList) searchElement(elem int) *Node{
+	for node := this.first; node != nil; node = node.next {
+		if node.element == elem {
+			return node
+		}
+	}
+	return nil
+}
+
+var linkedList = LinkedList{first: nil, last: nil, length: 0}
 
 func Add(elem int) {
+	linkedList.addElement(elem)
 	return
 }
 
 func Search(elem int) *Node {
-	return nil
+	return linkedList.searchElement(elem)
 }
