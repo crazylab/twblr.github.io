6bbe2a6e577ab9911647893917beaaf47f0e7659
Rahul <rahulna@thoughtworks.com>
2016-07-22 15:28:53 +0530

    if_structure code is added

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,18 @@
 package control_structures
 
+// import "strconv"
+import "fmt"
+
 func fizzBuzz(i int32) string {
-	return ""
+	if (i % 3 == 0) && (i % 5 == 0) {
+		return "FizzBuzz"
+	}
+	if i % 3 == 0 {
+		return "Fizz"
+	}
+	if i % 5 == 0 {
+		return "Buzz"
+	}	
+	// return strconv.Itoa(int(i))
+	return fmt.Sprintf("%d", i)
 }
