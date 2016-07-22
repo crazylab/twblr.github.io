df068f29d93648f8baa3048d78fa1b91824371be
Rahul <rahulna@thoughtworks.com>
2016-07-22 17:55:13 +0530

    PeopleWithCommonInterest code is added

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,20 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	result := []string{}
+	for people, interests:= range data{
+		if contains(interests, interest){
+			result = append(result, people)
+		}
+	}
+	return result
 }
 
 func contains(src []string, elem string) bool {
+	for _,value := range src{
+		if value == elem{
+			return true
+		}
+	}
 	return false
 }
