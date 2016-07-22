b3794a3d9e9cd7751164e7fd4170dbb980558aba
Rahul <rahulna@thoughtworks.com>
2016-07-22 16:04:12 +0530

    Added code in for_structure

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,18 @@
 package control_structures
 
+func isEven(n int) bool{
+	return n % 2 == 0
+}
+
 func collatzChainLength(n int) int {
-	return 0
+	count := 0
+	for n != 1 {
+		if isEven(n){
+			n = n / 2
+		} else{
+			n = 3 * n + 1
+		}
+		count++
+	}
+	return count
 }
