03a65ad2143787570ba2e89ce8539e1ade3e6c76
Rahul <rahulna@thoughtworks.com>
2016-07-23 10:25:41 +0530

    Shape code is added

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,29 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+type Square struct{
+	side int
+}
+
+func(aSquare Square) Area() int{
+	return aSquare.side * aSquare.side
+}
+
+type Rectangle struct{
+	length int
+	breadth int
+}
+
+func(aRectangle Rectangle) Area() int{
+	return aRectangle.length * aRectangle.breadth
+}
+
+type Hybrid struct{
+	square Square
+	rectangle Rectangle
+}
+
+func(aHybrid Hybrid) Area() int{
+	return aHybrid.square.Area() + aHybrid.rectangle.Area()
+}
